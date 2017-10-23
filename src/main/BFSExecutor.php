e4816cc51cd2821c9a7bf9580e888f83a33ce4d3
Terrence Howard <chemisus@gmail.com>
2017-10-23 17:35:35 -0400

    saving

--- src/main/BFSExecutor.php
+++ src/main/BFSExecutor.php
@@ -7,13 +7,11 @@ class BFSExecutor
     public function execute(Schema $schema, Query $query)
     {
         $root = new Node($schema, $schema->field($query->name()), $query);
-        $nodes = [];
         $queue = [$root];
 
         while (count($queue)) {
             $node = array_shift($queue);
 
-            array_push($nodes, $node);
             $children = $node->fetch();
             if (count($children)) {
                 array_push($queue, ...$children);
