c478b304da68f4b139b4085b9effdacbe6c2a889
Terrence Howard <chemisus@gmail.com>
2017-11-02 18:47:28 -0400

    saving

--- src/main/Types/InputObject.php
+++ src/main/Types/InputObject.php
@@ -7,6 +7,7 @@ use Chemisus\GraphQL\Type;
 use Chemisus\GraphQL\Types\Traits\DescriptionTrait;
 use Chemisus\GraphQL\Types\Traits\FieldsTrait;
 use Chemisus\GraphQL\Types\Traits\KindTrait;
+use Chemisus\GraphQL\Types\Traits\LeafTypeTrait;
 use Chemisus\GraphQL\Types\Traits\NameTrait;
 use Chemisus\GraphQL\Types\Traits\NullEnumValuesTrait;
 use Chemisus\GraphQL\Types\Traits\NullInputFieldsTrait;
@@ -23,6 +24,7 @@ class InputObjectType implements Type
     use NullOfTypeTrait;
     use NullEnumValuesTrait;
     use FieldsTrait;
+    use LeafTypeTrait;
 
     public function __construct(string $name)
     {
@@ -32,33 +34,5 @@ class InputObjectType implements Type
 
     public function resolve(Node $node, $parent, $value)
     {
-        if ($value === null) {
-            return null;
-        }
-
-        $object = (object) [];
-
-        foreach ($node->children($this->name) as $child) {
-            $name = $child->name();
-            $field = property_exists($value, $name) ? $value->{$name} : null;
-            $object->{$child->alias()} = $child->resolve($value, $field);
-        }
-
-        return $object;
-    }
-
-    public function type(Node $node, $value): Type
-    {
-        return $this;
-    }
-
-    public function types(Node $node, $values)
-    {
-        return [$this->name];
-    }
-
-    public function possibleTypes()
-    {
-        return [$this];
     }
 }
