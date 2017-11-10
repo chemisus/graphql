309b7f990176a8a3834c2c77dba32c5d6bad8733
Terrence Howard <chemisus@gmail.com>
2017-11-10 14:06:02 -0500

    cleaning up

--- src/main/DocumentBuilder.php
+++ src/main/DocumentBuilder.php
@@ -15,8 +15,6 @@ class DocumentBuilder
 
     private $parsed;
 
-    private $variables = [];
-
     private $source = <<< SOURCE
 scalar ID
 scalar String
@@ -40,23 +38,11 @@ type __Type {
     kind: __TypeKind!
     name: String
     description: String
-
-    # OBJECT and INTERFACE only
     fields(includeDeprecated: Boolean = false): [__Field!]
-
-    # OBJECT only
     interfaces: [__Type!]
-
-    # INTERFACE and UNION only
     possibleTypes: [__Type!]
-
-    # ENUM only
     enumValues(includeDeprecated: Boolean = false): [__EnumValue!]
-
-    # INPUT_OBJECT only
     inputFields: [__InputValue!]
-
-    # NON_NULL and LIST only
     ofType: __Type
 }
 
